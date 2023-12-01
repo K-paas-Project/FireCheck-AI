efe605ff09e7609cffcaab7e5de2af626e385c5d
yeseong0412 <yeseong0412@gmail.com>
2023-12-01 21:48:04 +0900

    Feat :: Add new video

--- test.py
+++ test.py
@@ -21,7 +21,7 @@ from ultralytics import YOLO
 
 def getVideoStreaming():
     # Load the YOLOv8 model
-    #model = YOLO('C:/Users/USER/Downloads/ultralytics-main/runs/detect/train35/weights/best.pt')
+    # model = YOLO('C:/Users/USER/Downloads/ultralytics-main/runs/detect/train35/weights/best.pt')
     model = YOLO('/app/runs/detect/train35/weights/best.pt')
 
     # 동영상 파일 사용시
@@ -42,9 +42,9 @@ def getVideoStreaming():
         annotated_frame = results[0].plot()
 
         # 결과가 발생하면 'event' 출력
-        if results[0].speed:
-            print('event')
-            yield "event: alarm\ndata: {}\n\n"
+        # if results[0].speed:
+        #     print('event')
+        #     yield "event: alarm\ndata: {}\n\n"
 
         # 이미지를 바이너리로 인코딩
         ret, buffer = cv2.imencode('.jpg', annotated_frame)
@@ -55,3 +55,91 @@ def getVideoStreaming():
                b'Content-Type: image/jpeg\r\n\r\n' + frame_bytes + b'\r\n')
 
 
+def getVideoStreaming1():
+    video_path1 = "/app/n1.mp4"
+
+    cap = cv2.VideoCapture(video_path1)
+
+    while cap.isOpened():
+        success, frame = cap.read()
+        if not success:
+            cap.set(cv2.CAP_PROP_POS_FRAMES, 0)
+        # 이미지를 바이너리로 인코딩
+        ret, buffer = cv2.imencode('.jpg', cap)
+        frame_bytes = bytearray(buffer.tobytes())
+
+        # 바이너리 이미지를 전송
+        yield (b'--frame\r\n'
+               b'Content-Type: image/jpeg\r\n\r\n' + frame_bytes + b'\r\n')
+
+
+def getVideoStreaming2():
+    video_path1 = "/app/n2.mp4"
+
+    cap = cv2.VideoCapture(video_path1)
+
+    while cap.isOpened():
+        success, frame = cap.read()
+        if not success:
+            cap.set(cv2.CAP_PROP_POS_FRAMES, 0)
+        # 이미지를 바이너리로 인코딩
+        ret, buffer = cv2.imencode('.jpg', cap)
+        frame_bytes = bytearray(buffer.tobytes())
+
+        # 바이너리 이미지를 전송
+        yield (b'--frame\r\n'
+               b'Content-Type: image/jpeg\r\n\r\n' + frame_bytes + b'\r\n')
+
+
+def getVideoStreaming3():
+    video_path1 = "/app/n3.mp4"
+
+    cap = cv2.VideoCapture(video_path1)
+
+    while cap.isOpened():
+        success, frame = cap.read()
+        if not success:
+            cap.set(cv2.CAP_PROP_POS_FRAMES, 0)
+        # 이미지를 바이너리로 인코딩
+        ret, buffer = cv2.imencode('.jpg', cap)
+        frame_bytes = bytearray(buffer.tobytes())
+
+        # 바이너리 이미지를 전송
+        yield (b'--frame\r\n'
+               b'Content-Type: image/jpeg\r\n\r\n' + frame_bytes + b'\r\n')
+
+
+def getVideoStreaming4():
+    video_path1 = "/app/n4.mp4"
+
+    cap = cv2.VideoCapture(video_path1)
+
+    while cap.isOpened():
+        success, frame = cap.read()
+        if not success:
+            cap.set(cv2.CAP_PROP_POS_FRAMES, 0)
+        # 이미지를 바이너리로 인코딩
+        ret, buffer = cv2.imencode('.jpg', cap)
+        frame_bytes = bytearray(buffer.tobytes())
+
+        # 바이너리 이미지를 전송
+        yield (b'--frame\r\n'
+               b'Content-Type: image/jpeg\r\n\r\n' + frame_bytes + b'\r\n')
+
+
+def getVideoStreaming5():
+    video_path1 = "/app/n5.mp4"
+
+    cap = cv2.VideoCapture(video_path1)
+
+    while cap.isOpened():
+        success, frame = cap.read()
+        if not success:
+            cap.set(cv2.CAP_PROP_POS_FRAMES, 0)
+        # 이미지를 바이너리로 인코딩
+        ret, buffer = cv2.imencode('.jpg', cap)
+        frame_bytes = bytearray(buffer.tobytes())
+
+        # 바이너리 이미지를 전송
+        yield (b'--frame\r\n'
+               b'Content-Type: image/jpeg\r\n\r\n' + frame_bytes + b'\r\n')
--- video.py
+++ video.py
@@ -1,20 +1,35 @@
 from fastapi import FastAPI
 from fastapi.responses import StreamingResponse
 
-from test import getVideoStreaming
-
-app = FastAPI()
-
-from fastapi import FastAPI
-from fastapi.responses import StreamingResponse
-from test import getVideoStreaming
+from test import getVideoStreaming, getVideoStreaming1, getVideoStreaming2, getVideoStreaming3, getVideoStreaming4, \
+    getVideoStreaming5
 
 app = FastAPI()
 
 @app.get("/")
 async def root():
-    return {"Hello": "PNP"}
+    return {"Hello": "LiVE ~!!"}
 
-@app.get("/video")
+@app.get("/fire")
 def video():
     return StreamingResponse(getVideoStreaming(), media_type="multipart/x-mixed-replace; boundary=frame")
+
+@app.get("/video1")
+def video():
+    return StreamingResponse(getVideoStreaming1(), media_type="multipart/x-mixed-replace; boundary=frame")
+
+@app.get("/video2")
+def video():
+    return StreamingResponse(getVideoStreaming2(), media_type="multipart/x-mixed-replace; boundary=frame")
+
+@app.get("/video3")
+def video():
+    return StreamingResponse(getVideoStreaming3(), media_type="multipart/x-mixed-replace; boundary=frame")
+
+@app.get("/video4")
+def video():
+    return StreamingResponse(getVideoStreaming4(), media_type="multipart/x-mixed-replace; boundary=frame")
+
+@app.get("/video5")
+def video():
+    return StreamingResponse(getVideoStreaming5(), media_type="multipart/x-mixed-replace; boundary=frame")
